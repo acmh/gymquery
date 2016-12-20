d6bdb6e6a7dd3b1bf831eea1259112243b69842b
jotapsl <jpsl@cin.ufpe.br>
2016-12-19 23:56:33 -0300

    View de usuários

--- public/app/Users/users.controller.js
+++ public/app/Users/users.controller.js
@@ -3,12 +3,81 @@
 
     angular
         .module("gymqueryApp")
-        .controller("userController", usrCtrl);
+        .controller("userController", userCtrl);
 
-    usrCtrl.$inject = [];
+    userCtrl.$inject = ["userService"];
 
-    function usrCtrl () {
+    function userCtrl (userService) {
+        var vm = this;
 
+        /* Models */
+        vm.enableGetMoreButton = true;
+        vm.searchInput = "";
+        vm.pageInfo = {};
 
+        vm.users = [
+            {
+                name: "nome1asdsad",
+                acc: 0,
+                tried: 3
+            },
+            {
+                name: "nome2",
+                acc: 1,
+                tried: 5
+            },
+            {
+                name: "nome2",
+                acc: 1,
+                tried: 5
+            },
+            {
+                name: "nome2",
+                acc: 1,
+                tried: 5
+            },
+            {
+                name: "nome2",
+                acc: 1,
+                tried: 5
+            },
+            {
+                name: "nome2",
+                acc: 1,
+                tried: 5
+            },
+            {
+                name: "nome2",
+                acc: 1,
+                tried: 5
+            },
+            {
+                name: "nome3",
+                acc: 10,
+                tried: 0
+            }
+        ];
+
+        /* Click Handlers */
+        vm.getMoreUsers = getMoreUsers;
+
+        /* Functions */
+
+        function getMoreUsers() {
+            vm.enableGetMoreButton = false;
+
+            searchParam.name = vm.searchInput;
+
+            userService.getQuestionList(searchParam, vm.pageInfo.page+1).then(
+                function(res) {
+                    vm.users = vm.users.concat(res.data.users.docs);
+                    vm.pageInfo.total = res.data.users.total;
+                    vm.pageInfo.page = parseInt(res.data.users.page);
+
+                    vm.enableGetMoreButton = true;
+                }
+                //TODO error callback
+            );
+        };
     };
 })();
--- public/app/Users/users.service.js
+++ public/app/Users/users.service.js
@@ -8,6 +8,23 @@
 	userServ.$inject = [];
 
 	function userServ () {
+		var service;
 
+		service = {
+			getUserList: getUserList
+		};
+
+		return service;
+
+		function getUserList(search, page) {
+			return $http({
+				method: "GET",
+				url: "/usersPaginated",
+				params: {
+					name: search.name,
+					page: page
+				}
+			});
+		};
 	};
 })();
